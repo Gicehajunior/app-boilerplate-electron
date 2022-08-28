f31908d662ee996e7153e8588daac828b5801343
Gicehajunior <gicehajunior76@gmail.com>
2022-08-28 09:14:16 +0300

    added helpers config

---
+++ app/Helpers/config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    'button_click_innerhtml_context': "Please Wait...",
+    'footer_app_name': "Electron <strong style='color: green'>Application Boilerplate</strong>",
+    'login_button_innerhtml_context':  "Sign In",
+    'register_button_innerhtml_context':  "Sign Up",
+    'forgot_password_button_innerhtml_context':  "Request Security Code",
+    'reset_password_button_innerhtml_context':  "Reset Password"
+}
+
+
+
